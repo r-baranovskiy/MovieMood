dc60ee35df48e6ac9dee167f1fa56750e0be28c2
koko6a <rus.baranovskiy@gmail.com>
2023-04-01 00:37:23 +0300

    project init

---
+++ MovieMood/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let rootVC = MainTabBarController()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = rootVC
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+}
+
---
+++ MovieMood/Controllers/Core/MainTabBarController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
