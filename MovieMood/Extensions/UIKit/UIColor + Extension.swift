2a2cae77a75fab1923c0cb86dd66ee03ddb45135
koko6a <rus.baranovskiy@gmail.com>
2023-04-03 11:40:02 +0300

    add colors

---
+++ MovieMood/Extensions/UIKit/UIColor + Extension.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+extension UIColor {
+    /// Convenience name for uses color F.E. UIColor.custom.mainBackground
+    class var custom: CustomColor { return CustomColor() }
+    
+    /// Returns UIColor from assets
+    struct CustomColor {
+        var mainBackground: UIColor {
+            return UIColor(named: "Main-Background") ?? .systemBackground
+        }
+        
+        var goldColor: UIColor {
+            return UIColor(named: "Gold") ?? UIColor(red: 0.612, green: 0.643,
+                                                     blue: 0.671, alpha: 1)
+        }
+        
+        var mainBlue: UIColor {
+            return UIColor(named: "Main-Blue") ?? .systemBlue
+        }
+    }
+}
