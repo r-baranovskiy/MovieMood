f45765f4585948d35af872530d6a488bc816ac85
koko6a <rus.baranovskiy@gmail.com>
2023-04-05 13:21:00 +0300

    create user model, create sign up method

--- MovieMood/Services/AuthManager.swift
+++ MovieMood/Services/AuthManager.swift
@@ -1,5 +1,46 @@
 import Foundation
+import FirebaseAuth
 
+/// Manager responsible for signing in, up, and out
 final class AuthManager {
+    /// Singleton instance of the manager
+    static let shared = AuthManager()
     
+    /// Attempt to sign up
+    /// - Parameters:
+    ///   - email: User email
+    ///   - password: User password
+    ///   - confirmPassword: User confirm password
+    ///   - completion: Callback that returns MovieUser or result
+    func createUser(email: String?, password: String?, confirmPassword: String?,
+                    completion: @escaping (Result<MovieUser, Error>) -> Void) {
+        guard let email = email, let password = password,
+              let confirmPassword = confirmPassword, email != "",
+              password != "", confirmPassword != "" else {
+            completion(.failure(AuthError.notFilled))
+            return
+        }
+        
+        guard password == confirmPassword else {
+            completion(.failure(AuthError.passwordNotMatched))
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email,
+                               password: password) { result, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            if let result = result {
+                if let email = result.user.email {
+                    let user = MovieUser(id: result.user.uid, email: email)
+                    completion(.success(user))
+                } else {
+                    completion(.failure(AuthError.unknownError))
+                }
+            }
+        }
+    }
 }
