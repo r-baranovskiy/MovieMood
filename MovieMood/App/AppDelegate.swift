b786cefe1af369555ecf1ba2ae8b8b4374cd7907
koko6a <rus.baranovskiy@gmail.com>
2023-04-05 13:00:27 +0300

    add firebase

--- MovieMood/App/AppDelegate.swift
+++ MovieMood/App/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,6 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         let rootVC = SignInViewController()
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = rootVC
