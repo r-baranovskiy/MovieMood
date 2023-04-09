ddab32652e065a19ef3ce195b1620fb49b1d78b4
koko6a <rus.baranovskiy@gmail.com>
2023-04-09 14:28:15 +0300

    edit

--- MovieMood/App/AppDelegate.swift
+++ MovieMood/App/AppDelegate.swift
@@ -10,15 +10,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = DetailViewController()
-//        AuthManager.shared.fetchCurrentMovieUser { result in
-//            switch result {
-//            case .success(let movieUser):
-//                window.rootViewController = MainTabBarController(user: movieUser)
-//            case .failure:
-//                window.rootViewController = SignInViewController()
-//            }
-//        }
+        AuthManager.shared.fetchCurrentMovieUser { result in
+                switch result {
+                case .success(let movieUser):
+                    window.rootViewController = MainTabBarController(user: movieUser)
+                case .failure:
+                    window.rootViewController = SignInViewController()
+                }
+            }
 
         window.makeKeyAndVisible()
         self.window = window
