1571c04a2fe24093b65b1bff2893e6efd65b5c19
koko6a <rus.baranovskiy@gmail.com>
2023-04-13 22:56:18 +0300

    complete refactoring

--- MovieMood/Controllers/Core/SearchViewController.swift
+++ MovieMood/Controllers/Core/SearchViewController.swift
@@ -2,7 +2,23 @@ import UIKit
 
 final class SearchViewController: UIViewController {
     
-    // MARK: - Collection View
+    // MARK: - Properties
+    
+    private var movies = [MovieDetail]()
+    
+    private let apiManager = ApiManager(
+        networkManager: NetworkManager(jsonService: JSONDecoderManager())
+    )
+    
+    private var moviesID: [Int] = []
+    
+    private lazy var tapGesture: UITapGestureRecognizer = {
+        return UITapGestureRecognizer(
+            target: view,action: #selector(view.endEditing)
+        )
+    }()
+        
+    //MARK: - Views
     
     private lazy var movieColletionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -22,36 +38,15 @@ final class SearchViewController: UIViewController {
         return collection
     }()
     
-    //MARK: - Private Properties
     private let searchTextField = MovieSearchTextField()
-    private let crossButton = CrossButton()
-    private let filterButton = FilterButton()
     private let filterPopupView = FilterPopupView()
-    
-    private var movies = [MovieDetail]()
-    private let apiManager = ApiManager(
-        networkManager: NetworkManager(jsonService: JSONDecoderManager())
-    )
-    
-    private let searchImageView: UIImageView = {
-        let view = UIImageView(image: UIImage(named: "search")?.withTintColor(.label))
-        view.heightAnchor.constraint(equalToConstant: 16).isActive = true
-        view.widthAnchor.constraint(equalToConstant: 16).isActive = true
-        return view
-    }()
-    
-    
-    private var moviesID: [Int] = []
-    
-    private lazy var tapGesture = UITapGestureRecognizer(target: view, action: #selector(view.endEditing))
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         filterPopupView.delegate = self
         searchTextField.delegate = self
-        setupSearchUI()
-        addAction()
+        searchTextField.searchFieldDelegate = self
         setupCollectionView()
         //fetchMovies()
     }
@@ -74,32 +69,34 @@ final class SearchViewController: UIViewController {
         }
     }
     
-    //MARK: - Private Methodes
-    private func addAction() {
-        crossButton.addTarget(self, action: #selector(crossButtonTapped), for: .touchUpInside)
-        filterButton.addTarget(self, action: #selector(filterButtonTapped), for: .touchUpInside)
-    }
-    
-    @objc private func crossButtonTapped() {
-        searchTextField.text = ""
-        searchTextField.endEditing(true)
-    }
-    
-    @objc private func filterButtonTapped() {
-        let heightTabBar = tabBarController?.tabBar.frame.height ?? 0
-        view.addSubview(filterPopupView)
-        filterPopupView.frame = CGRect(x: 0, y: view.frame.height, width: view.frame.width, height: view.frame.height / 2)
-        UIView.animate(withDuration: 0.3) {
-            self.filterPopupView.frame.origin.y -= (self.view.frame.height / 2) + heightTabBar
-        }
-    }
-    
     private func hideFilter() {
         let heightTabBar = tabBarController?.tabBar.frame.height ?? 0
         UIView.animate(withDuration: 0.3) {
             self.filterPopupView.frame.origin.y += (self.view.frame.height / 2) + heightTabBar
         } completion: { _ in
             self.filterPopupView.removeFromSuperview()
+        }
+    }
+}
+
+// MARK: - MovieSearchTextFieldDelegate
+
+extension SearchViewController: MovieSearchTextFieldDelegate {
+    func didTapCrossButton() {
+        searchTextField.text = nil
+        searchTextField.endEditing(true)
+    }
+    
+    func didTapFilterButton() {
+        let heightTabBar = tabBarController?.tabBar.frame.height ?? 0
+        view.addSubview(filterPopupView)
+        filterPopupView.frame = CGRect(
+            x: 0, y: view.frame.height, width: view.frame.width,
+            height: view.frame.height / 2
+        )
+        UIView.animate(withDuration: 0.3) {
+            self.filterPopupView.frame.origin.y -= (
+                self.view.frame.height / 2) + heightTabBar
         }
     }
 }
@@ -208,7 +205,19 @@ extension SearchViewController {
     private func setupCollectionView() {
         movieColletionView.backgroundColor = .none
         view.backgroundColor = .custom.mainBackground
-        view.addSubviewWithoutTranslates(movieColletionView)
+        view.addSubviewWithoutTranslates(searchTextField, movieColletionView)
+        
+        NSLayoutConstraint.activate([
+            searchTextField.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 35
+            ),
+            searchTextField.leftAnchor.constraint(
+                equalTo: view.leftAnchor, constant: 24
+            ),
+            searchTextField.rightAnchor.constraint(
+                equalTo: view.rightAnchor, constant: -24
+            )
+        ])
         
         NSLayoutConstraint.activate([
             movieColletionView.topAnchor.constraint(
@@ -225,45 +234,4 @@ extension SearchViewController {
             )
         ])
     }
-    
-    private func setupSearchUI() {
-        view.addSubviewWithoutTranslates(
-            searchTextField, crossButton, filterButton, searchImageView
-        )
-        
-        NSLayoutConstraint.activate([
-            searchTextField.topAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 35
-            ),
-            searchTextField.leftAnchor.constraint(
-                equalTo: view.leftAnchor, constant: 24
-            ),
-            searchTextField.rightAnchor.constraint(
-                equalTo: view.rightAnchor, constant: -24
-            ),
-            
-            filterButton.rightAnchor.constraint(
-                equalTo: searchTextField.rightAnchor, constant: -19
-            ),
-            filterButton.centerYAnchor.constraint(
-                equalTo: searchTextField.centerYAnchor
-            ),
-            
-            crossButton.rightAnchor.constraint(
-                equalTo: searchTextField.rightAnchor, constant: -55
-            ),
-            crossButton.centerYAnchor.constraint(
-                equalTo: searchTextField.centerYAnchor
-            ),
-            
-            searchImageView.leftAnchor.constraint(
-                equalTo: searchTextField.leftAnchor, constant: 18
-            ),
-            searchImageView.centerYAnchor.constraint(
-                equalTo: searchTextField.centerYAnchor
-            )
-        ])
-    }
 }
-
-
--- MovieMood/Views/TextFields/MovieSearchTextField.swift
+++ MovieMood/Views/TextFields/MovieSearchTextField.swift
@@ -1,24 +1,71 @@
-//
-//  MovieSearchTextField.swift
-//  MovieMood
-//
-//  Created by Дмитрий on 10.04.2023.
-//
-
 import UIKit
 
+protocol MovieSearchTextFieldDelegate: AnyObject {
+    func didTapCrossButton()
+    func didTapFilterButton()
+}
+
 final class MovieSearchTextField: UITextField {
+    
+    weak var searchFieldDelegate: MovieSearchTextFieldDelegate?
+    
+    // MARK: - Views
+    
+    private let crossButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "cross")?
+            .withTintColor(.label), for: .normal)
+        button.heightAnchor.constraint(equalToConstant: 10).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 10).isActive = true
+        return button
+    }()
+    
+    private let filterButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "filter")?
+            .withTintColor(.label), for: .normal)
+        button.heightAnchor.constraint(equalToConstant: 13).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 13).isActive = true
+        return button
+    }()
+    
+    private let searchImageView: UIImageView = {
+        let view = UIImageView(image: UIImage(named: "search")?
+            .withTintColor(.secondaryLabel))
+        view.heightAnchor.constraint(equalToConstant: 16).isActive = true
+        view.widthAnchor.constraint(equalToConstant: 16).isActive = true
+        return view
+    }()
     
     private let padding = UIEdgeInsets(top: 0, left: 42, bottom: 0, right: 75)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupTextField()
+        setTargets()
     }
     
-    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Behaviour
+    
+    private func setTargets() {
+        filterButton.addTarget(self, action: #selector(didTapFilterButton),
+                               for: .touchUpInside)
+        crossButton.addTarget(self, action: #selector(didTapCrossButton),
+                              for: .touchUpInside)
+    }
+    
+    @objc
+    private func didTapFilterButton() {
+        searchFieldDelegate?.didTapFilterButton()
+    }
+    
+    @objc
+    private func didTapCrossButton() {
+        searchFieldDelegate?.didTapCrossButton()
     }
     
     //MARK: - Ovverade Methods
@@ -47,5 +94,29 @@ final class MovieSearchTextField: UITextField {
         layer.borderWidth = 1
         layer.borderColor = UIColor.custom.mainBlue.cgColor
         backgroundColor = .clear
+        
+        addSubviewWithoutTranslates(crossButton, filterButton, searchImageView)
+        NSLayoutConstraint.activate([
+            crossButton.trailingAnchor.constraint(
+                equalTo: self.trailingAnchor, constant: -55
+            ),
+            crossButton.centerYAnchor.constraint(
+                equalTo: self.centerYAnchor
+            ),
+            
+            filterButton.trailingAnchor.constraint(
+                equalTo: self.trailingAnchor, constant: -19
+            ),
+            filterButton.centerYAnchor.constraint(
+                equalTo: self.centerYAnchor
+            ),
+            
+            searchImageView.leadingAnchor.constraint(
+                equalTo: self.leadingAnchor, constant: 18
+            ),
+            searchImageView.centerYAnchor.constraint(
+                equalTo: self.centerYAnchor
+            )
+        ])
     }
 }
